2e943890c18ba390e7938ab00c3475d1ce46c365
DungChan <garrickedd@gmail.com>
2024-10-22 01:55:28 +0700

    feat: update Services

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SellphoneC.Models;
+
+namespace SellphoneC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
