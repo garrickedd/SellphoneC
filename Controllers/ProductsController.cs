8e31dd7c9682375ca5205a5a97f10816fb570968
DungChan <garrickedd@gmail.com>
2024-10-22 01:53:47 +0700

    feat: update controller for product

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SellphoneC.Services;
+
+namespace SellphoneC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+    }
+}
