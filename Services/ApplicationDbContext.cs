b9b907d906c5c2f421ef3b4439a9f6da1f787b89
Dung Tran <garrickedd@gmail.com>
2024-10-29 02:13:43 +0700

    feat: CRU on products

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using SellphoneC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SellphoneC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions option) : base(option)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
