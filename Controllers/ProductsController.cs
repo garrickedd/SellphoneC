54e3d2d9056e1b5aad95ea0a870d69ec32435e62
Dung Tran <garrickedd@gmail.com>
2024-10-29 02:47:17 +0700

    feat: add Search funtionality

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -16,9 +16,15 @@ namespace SellphoneC.Controllers
             this.context = context;
             this.environment = environment;
         }
-        public IActionResult Index(int pageIndex)
+        public IActionResult Index(int pageIndex, string? search)
         {
             IQueryable<Product> query = context.Products;
+
+            // search functionality
+            if (search != null)
+            {
+                query = query.Where(p => p.Name.Contains(search) || p.Brand.Contains(search));
+            }
 
             query = query.OrderByDescending(p => p.Id);
 
@@ -37,6 +43,8 @@ namespace SellphoneC.Controllers
 
             ViewData["PageIndex"] = pageIndex;
             ViewData["TotalPages"] = totalPages;
+
+            ViewData["Search"] = search ?? "";
 
             return View(products);
         }
