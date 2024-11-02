70c743e24cb752cadc73942f34f4941e5f4aa474
Dung Tran <garrickedd@gmail.com>
2024-11-03 02:40:34 +0700

    feat: create the Register Form

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SellphoneC.Models;
+
+namespace SellphoneC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager) 
+        {
+            this.signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
