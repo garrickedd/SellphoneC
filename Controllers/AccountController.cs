4170e567a6dedba5926e2103cf4d318251c6b5eb
Dung Tran <garrickedd@gmail.com>
2024-11-03 03:21:16 +0700

    feat: user registration - create user account

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,16 +6,60 @@ namespace SellphoneC.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly SignInManager<ApplicationUser> signInManager;
 
         public AccountController(UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager) 
         {
+            this.userManager = userManager;
             this.signInManager = signInManager;
         }
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View(registerDto);
+            }
+
+            // create a new account and authenticate the user
+            var user = new ApplicationUser()
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                UserName = registerDto.Email, // UserName will be used to authenticate the user
+                Email = registerDto.Email,
+                PhoneNumber = registerDto.PhoneNumber,
+                Address = registerDto.Address,
+                CreatedAt = DateTime.Now,
+            };
+
+            var result = await userManager.CreateAsync(user, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                // successful user registration
+                await userManager.AddToRoleAsync(user, "client");
+
+                // sign in the new user
+                await signInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            // registration failed => show registration errors
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(registerDto);
         }
     }
 }
